687b9bc2040a4d91e508563273fa5b566f103364
Irtiqa Malik <Irtiqa.m02@gmail.com>
2024-09-29 21:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (92.02%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -11,9 +11,6 @@ class Solution {
             }
             
         }
-        if(start==arr.length){
-            return arr[0];
-        }
-        return arr[start];
+        return arr[start % arr.length];
     }
 }
