d2e02e6d7e309f70f4b88e571e506cdfb7e92dd5
Irtiqa Malik <Irtiqa.m02@gmail.com>
2024-10-25 22:34:43 +0530

    Time: 4 ms (21.11%) | Memory: 47.4 MB (11.88%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+
+        // take first element and find difference with target
+        // using the difference as key do BS in remaining array
+        int difference =0;
+        for(int i=0; i< arr.length;i++){
+            difference = target-arr[i];
+            int potentialAns = binarySearch(i+1,arr.length-1,arr,difference);
+            if(potentialAns != -1){
+                return new int[]{i+1,potentialAns+1};
+            }
+            
+        }
+        return new int[]{-1,-1};
+    }
+
+    private int binarySearch(int start, int end , int[] arr, int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[mid]>target){
+                end = mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
