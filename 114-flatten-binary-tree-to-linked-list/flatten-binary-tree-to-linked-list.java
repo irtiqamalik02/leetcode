8c7a9bbbd0a1e0f497c816b192c25f1ac7df326f
Irtiqa Malik <Irtiqa.m02@gmail.com>
2024-11-21 23:06:59 +0530

    Time: 1 ms (22.12%) | Memory: 41.9 MB (82.32%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -14,20 +14,25 @@
  * }
  */
 class Solution {
-    private TreeNode prev = null;
-
+    Queue<TreeNode> queue = new LinkedList<>();
     public void flatten(TreeNode root) {
-        if (root == null) return;
+        if(root == null ) return;
 
-        // Post-order traversal: Right -> Left -> Root
-        flatten(root.right);
-        flatten(root.left);
+        preOrder(root);
+        if(queue.size()== 1) return;
+        while(!queue.isEmpty()){
+            root.right = queue.poll();
+            root.left = null;
+            root = root.right;
+        }
 
-        // Modify pointers to flatten the tree
-        root.right = prev;
-        root.left = null;
-
-        // Move the prev pointer
-        prev = root;
     }
+    private void preOrder(TreeNode root) {
+        if(root == null) return;
+        queue.add(root);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+
 }
