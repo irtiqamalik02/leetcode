61db9e8dc820b7b4835a603f8f285686dd19e5da
Irtiqa Malik <Irtiqa.m02@gmail.com>
2024-11-21 23:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (69.19%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,38 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    Queue<TreeNode> queue = new LinkedList<>();
+    /**
+     * - at each step we will think how to make left child null during our DLR
+     * traversal as reqd by question
+     * -
+     * 
+     */
     public void flatten(TreeNode root) {
-        if(root == null ) return;
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) { 
+                TreeNode temp = current.left;
+                while (temp.right != null) {
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
 
-        preOrder(root);
-        if(queue.size()== 1) return;
-        while(!queue.isEmpty()){
-            root.right = queue.poll();
-            root.left = null;
-            root = root.right;
+            current = current.right;
         }
 
     }
-    private void preOrder(TreeNode root) {
-        if(root == null) return;
-        queue.add(root);
-        preOrder(root.left);
-        preOrder(root.right);
-    }
-
 
 }
